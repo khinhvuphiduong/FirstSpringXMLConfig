17cdbabc3234be755ad5d982b3b4f8583a840ffc
Nguyen Huy Khanh <khanhnguyenhuy@hotmail.com>
2016-08-11 09:52:32 +0700

    ok

---
+++ source/main/java/khanhnh/demo/GreetingServiceImpl.java
@@ -0,0 +1,11 @@
+package khanhnh.demo;
+
+public class GreetingServiceImpl implements GreetingService {
+
+	@Override
+	public String getGreeting(String name) {
+		// TODO Auto-generated method stub
+		return "Hello " + name + "!";
+	}
+
+}
